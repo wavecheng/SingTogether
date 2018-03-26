9c3e74d23dace7ae655b08651adfcdf1df93cdfd
Bo Chen <bo.chen@citrix.com>
2018-03-26 12:28:20 +0800

    spring boot template

---
+++ src/main/java/com/wavecheng/sing/SingApplication.java
@@ -0,0 +1,12 @@
+package com.wavecheng.sing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wavecheng/sing/SingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wavecheng.sing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
